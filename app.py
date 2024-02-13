95fabee84cab12b81da163ee76115784869a29e2
Shih Hsien Ma <100989742+SH-Mars@users.noreply.github.com>
2024-02-13 10:40:05 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -2,13 +2,42 @@ import streamlit as st
 import pandas as pd
 import re
 import datetime as dt
+import sqlite3 as sqlite
+from sqlite3 import Error
 
+# Basic layout of the page
 st.set_page_config(page_title='Exp Date Verification ✔️')
 st.title('Expiration Date Validation ✔️')
 
 st.write('Scan the barcode on the Case Label')
 
-barcode = st.text_input('Barcode')
+# ------------------------------------------------------------------
+# Create the SQL connection to db
+def create_connection(db_file):
+    
+    conn = None
+    
+    try:
+        conn = sqlite.connect(db_file)
+
+    except Error as e:
+        print(e)
+    return conn
+
+conn = create_connection('first_label_scan.db')
+cur = conn.cursor()
+# cur.execute("DROP TABLE IF EXISTS scan_record")
+# ------------------------------------------------------------------
+
+def insertData(a, b, c, d, e):
+    cur.execute("""CREATE TABLE IF NOT EXISTS scan_record (scan_time DATETIME, item_gtin nvarchar(14), lot nvarchar(4), exp_date nvarchar(6), if_pass nvarchar(3));""")
+    cur.execute("""INSERT INTO scan_record VALUES(?, ?, ?, ?, ?)""", (a, b, c, d, e))
+    conn.commit()
+    
+# ------------------------------------------------------------------
+
+placeholder = st.empty()
+barcode = placeholder.text_input('Barcode', key='Barcode')
 
 # Define patterns for each number you want to extract
 gtin_pattern = re.compile(r'\(01\)(\d+)')
@@ -33,17 +62,30 @@ today = dt.date.today()
 
 corr_exp = dt.date(today.year + 3, today.month, 1).strftime('%y%m%d')
 
+scan_time = dt.datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+
 check_button = st.button('Check', help='Click to verify the expiration date')
 
 if check_button:
     if exp == corr_exp:
+        if_pass = "Yes"
+        insertData(scan_time, gtin, lot, exp, if_pass)
         st.markdown(f'✅ Lot: {lot} has the correct expiration date on the label.')
         st.success(f"Validation successful!")
+        
     else:
+        if_pass = "No"
+        insertData(scan_time, gtin, lot, exp, if_pass)
         st.warning('Double Check the Expiration Date', icon="⚠️")
         st.error(f"Expiration Date: {dt.date(today.year + 3, today.month, 1).strftime('%Y-%m-%d')}")
 
-reset_button = st.button('Reset', type='primary')
+def clear_barcode():
+    st.session_state['Barcode'] = ""
 
-if reset_button:
-    st.experimental_rerun()
+reset_button = st.button('Reset', type='primary', on_click=clear_barcode)
+
+#if reset_button:
+#    barcode = placeholder.text_input('Barcode', value='', key=2)
+#    st.rerun()
+
+st.write("Reach out to QA Dept for any needs to review the scanning records.")
