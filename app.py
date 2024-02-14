7d8063c1e9fcf0adead539aa751d43cfea8b8d96
Shih Hsien Ma <100989742+SH-Mars@users.noreply.github.com>
2024-02-13 16:52:27 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def insert_data_into_sql_server(a, b, c, d, e):
     
     try:
         # Create the SQL connection to db
-        connection_string = 'DRIVER={SQL Server};SERVER=TEST-VM04;DATABASE=first_label_scan;UID=APLUS_NT\shihhsienma;Trusted_Connection=yes;'
+        connection_string = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=TEST-VM04;DATABASE=first_label_scan;UID=APLUS_NT\shihhsienma;Trusted_Connection=yes;'
 
         # Establish a connection to the database
         conn = pyodbc.connect(connection_string)
