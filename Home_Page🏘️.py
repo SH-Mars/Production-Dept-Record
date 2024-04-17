3d1ae3b882f9fa9f4175ace2b7ffa4042898471b
Shih Hsien Ma <100989742+SH-Mars@users.noreply.github.com>
2024-04-17 16:53:25 -0700

    Update Home_Page🏘️.py

--- Home_Page🏘️.py
+++ Home_Page🏘️.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 def main():
-    st.title("Production Dept Home Page 🏘️")
-    st.subheader("Stryker Case Label Expiration Validation ✔️")
+    st.title("Production Expiration Date & Packaging Material Usage Inspection and Monitoring System Home Page 🏘️")
+    st.subheader("Stryker Case Label Expiration Verification ✔️")
     st.subheader("Packaging Roll Material Tracing 📊")
     
     st.write("Please refer to the sidebar to direct to system page.")
