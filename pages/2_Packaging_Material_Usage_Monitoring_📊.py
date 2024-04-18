4699ee1036976abeab4edc73cb6770b8eee70177
Shih Hsien Ma <100989742+SH-Mars@users.noreply.github.com>
2024-04-18 09:47:19 -0700

    Update 2_Packaging_Material_Usage_Monitoring_📊.py
    added connection to mongodb

--- pages/2_Packaging_Material_Usage_Monitoring_📊.py
+++ pages/2_Packaging_Material_Usage_Monitoring_📊.py
@@ -2,6 +2,9 @@ import streamlit as st
 from reportlab.lib import colors
 from reportlab.platypus import SimpleDocTemplate, Image, Table, TableStyle
 import io
+import pytz
+import datetime as dt
+import pymongo
 import base64
 
 # -----------------------------------------------------------------
@@ -18,7 +21,16 @@ def login():
         else:
             st.error("Invalid credentials")
 # ------------------------------------------------------------------
+# MongoDB connection
+username = st.secrets["mongo_username"]
+password = st.secrets["mongo_password"]
+CONNECTION_STRING = f"mongodb+srv://{username}:{password}@cluster.89jetih.mongodb.net/"
 
+client = pymongo.MongoClient(CONNECTION_STRING)
+db = client["first_label_scan"]
+collection = db['packaging_material']
+
+# ------------------------------------------------------------------
 # generate PDF file for label printint
 def generate_pdf(image_path, user_data, image_width=135, image_height=115):
     
@@ -39,7 +51,7 @@ def generate_pdf(image_path, user_data, image_width=135, image_height=115):
     for key, value in user_data.items():
         data.append([key, value])
         
-    table = Table(data)
+    table = Table(data, hAlign='CENTER')
     table_style = TableStyle([('BACKGROUND', (0,0), (-1,0), colors.grey),
                               ('TEXTCOLOR',(0,0),(-1,0),colors.whitesmoke),
                               ('ALIGN', (0,0), (-1,-1), 'CENTER'),  # Alignment
@@ -83,6 +95,9 @@ def main():
         material_type = st.radio('Material Type', options)
         st.write("")
         data = {}
+
+        # set timezone for scan time
+        tzInfo = pytz.timezone('America/Los_Angeles')
         
         st.write(r"$\textsf{\Large Scan the Barcode on the Roll Packaging Label Accordingly}$")
     
@@ -92,6 +107,7 @@ def main():
         
             if data["Part Number"] == "RS-T440-1":
                 image_path = "images/amcor.png"
+                data["Scan Time"] = dt.datetime.now(tz=tzInfo).strftime('%Y-%m-%d %H:%M:%S')
                 data["Roll No"] = st.text_input('Roll Number')
                 data["Batch Number"] = st.text_input('Batch Number')
                 data["MSI"] = st.text_input('MSI')
@@ -99,12 +115,13 @@ def main():
             
             else:
                 image_path = "images/oliver.png"
-        
+                data["Scan Time"] = dt.datetime.now(tz=tzInfo).strftime('%Y-%m-%d %H:%M:%S')
                 data["PO Number"] = st.text_input('PO Number')
                 data["Part Desc"] = 'APLS 440mm 0C SL 1059B/27HT-2C'
                 data["Sales Order"] = st.text_input('Sales Order')
                 data["Material Number"] = st.text_input('Material #')
                 data["Mfg Date"] = st.text_input('Mfg Date')
+                data['Qty'] = st.text_input('Qty')
                 id = st.text_input('HU ID')
                 data["HU ID"] = id[10:]
         
@@ -116,13 +133,17 @@ def main():
                 pdf_buffer = generate_pdf(image_path, data, 135, 115)
             
                 pdf_base64 = base64.b64encode(pdf_buffer.read()).decode('utf-8')
-                pdf_display = f'<embed src="data:application/pdf;base64,{pdf_base64}" width="700" height="500" type="application/pdf">'
+                pdf_display = f'<embed src="data:application/pdf;base64,{pdf_base64}" width="700" height="700" type="application/pdf">'
                 st.markdown(pdf_display, unsafe_allow_html=True)
                 st.success('PDF generated successfully!')
+
+                # insert record into db
+                collection.insert_one(data)
                 
         elif material_type == 'Soft Pack':
             image_path = "images/amcor.png"
             data["Item Number"] = st.text_input('Item No')
+            data["Scan Time"] = dt.datetime.now(tz=tzInfo).strftime('%Y-%m-%d %H:%M:%S')
             data["Roll No"] = st.text_input('Roll Number')
             data["Batch Number"] = st.text_input('Batch Number')
             data["MSI"] = st.text_input('MSI')
@@ -138,12 +159,16 @@ def main():
                 pdf_display = f'<embed src="data:application/pdf;base64,{pdf_base64}" width="700" height="500" type="application/pdf">'
                 st.markdown(pdf_display, unsafe_allow_html=True)
                 st.success('PDF generated successfully!')
+
+                # insert record into db
+                collection.insert_one(data)
         
         else:
             image_path = "images/primex.png"
             item_number = 'RS-H460-2'
             st.subheader(f"Part Number: {item_number}")
             data["Item Number"] = item_number
+            data["Scan Time"] = dt.datetime.now(tz=tzInfo).strftime('%Y-%m-%d %H:%M:%S')
             info = st.text_input('Scan Barcode')
         
             if info != "":
@@ -160,6 +185,9 @@ def main():
                 pdf_display = f'<embed src="data:application/pdf;base64,{pdf_base64}" width="700" height="500" type="application/pdf">'
                 st.markdown(pdf_display, unsafe_allow_html=True)
                 st.success('PDF generated successfully!')
+
+                # insert record into db
+                collection.insert_one(data)
             
 if __name__ == "__main__":
     table_data = []  # Initialize table data
